a0ede12b0e166f4a5d2a8032172cec66112a9f48
Matt D <MDes41@users.noreply.github.com>
2017-06-20 19:02:46 -0600

    Grep the url

--- video.sh
+++ video.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #my first script
+#symbolic link ln -s /path/to/file /path/to/link
 
 echo 'Hello World!'
+arg=`echo "$1" | sed "s|https://|http://|"`
+echo $arg
+wget -q -O - "$arg" > y.htm
+grep "url=" y.htm | tr '"' '\n'
